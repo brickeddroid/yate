d14f7932e016a15ac487bb3732e3750754615ffc
Erik Gunther <eguenther89@gmail.com>
2024-04-14 16:30:22 +0200

    Add unit test for logging mechanism

--- unittests/utils/test.cpp
+++ unittests/utils/test.cpp
@@ -1,5 +1,13 @@
 #include <gtest/gtest.h>
+#include <log.hpp>
 #include <threadsafequeue.hpp>
+
+TEST(UtilsTest, LogAssertion){
+    // Log_t::NONE prints message without prefix (timestamp, domain, level)
+    int result = Yate::Utils::log(Yate::Utils::Log_t::NONE, "DOMAIN", "Hello %s", "World");
+
+    EXPECT_EQ(strlen("Hello World"), result);
+}
 
 TEST(UtilsTest, QueueAssertions) {
     Yate::Utils::ThreadSafeQueue<int> q;
