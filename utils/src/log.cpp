ea389af618b027136bbd3207773b7bac348243ce
Erik Gunther <eguenther89@gmail.com>
2024-04-21 21:19:19 +0200

    Add colorized log messages for console output

--- utils/src/log.cpp
+++ utils/src/log.cpp
@@ -5,35 +5,43 @@
 
 namespace Yate::Utils {
 
-const std::map<Log_t, const std::string> log_level_to_string = {
-    { Log_t::SILLY,   "SILLY" },
-    { Log_t::VERBOSE, "VERBOSE" },
-    { Log_t::DEBUG,   "DEBUG" },
-    { Log_t::INFO,    "INFO" },
-    { Log_t::WARNING, "WARNING" },
-    { Log_t::ERROR,   "ERROR" },
-    { Log_t::NONE,    "NONE" }
+/* Used code colors can be found here:
+ * https://www.codeproject.com/Tips/5255355/How-to-Put-Color-on-Windows-Console
+*/
+const std::map<Log_t, std::pair<const std::string, const std::string>> log_level_to_string = {
+    { Log_t::SILLY,   std::make_pair("SILLY", "\033[90m") },
+    { Log_t::VERBOSE, std::make_pair("VERBOSE", "\033[32m") },
+    { Log_t::DEBUG,   std::make_pair("DEBUG", "\033[92m") },
+    { Log_t::INFO,    std::make_pair("INFO", "\033[37m") },
+    { Log_t::WARNING, std::make_pair("WARNING", "\033[93m") },
+    { Log_t::ERROR,   std::make_pair("ERROR", "\033[91m") },
+    { Log_t::NONE,    std::make_pair("NONE", "") }
 };
 
+constexpr const std::string default_console_color = "\033[0m";
 Log_t LogLevel = Log_t::SILLY;
 
-std::mutex log_mutex;
-
 int log(Log_t log_level, const std::string& domain, const std::string& format, ...){
-    const std::lock_guard<std::mutex> lock(log_mutex);
-    if(log_level < LogLevel) return 0;
+    //const std::lock_guard<std::mutex> lock(log_mutex);
+    if( log_level < LogLevel ||
+        log_level > Log_t::NONE){
+        return 0;
+    }
     std::stringstream ss;
+    ss << log_level_to_string.at(log_level).second;
+    int color_len = log_level_to_string.at(log_level).second.length();
     if(log_level != Log_t::NONE){
         ss << "["
            << std::chrono::floor<std::chrono::milliseconds>(std::chrono::system_clock::now())
            << "]["
-           << log_level_to_string.at(log_level)
+           << log_level_to_string.at(log_level).first
            << "]["
            << domain
            << "] ";
     }
-    ss << format;
-    int done = 0;
+    ss << format << default_console_color;
+    color_len += default_console_color.length();
+    int done = -color_len;
     va_list arg;
     va_start(arg, ss.str().c_str());
     done = vfprintf(stdout, ss.str().c_str(), arg);
