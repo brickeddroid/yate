5d43710669730843b159992669f4f751a2cbaefa
Erik Gunther <eguenther89@gmail.com>
2024-04-21 21:36:22 +0200

    Refactoring of DocumentHandler
    Separate communication from the internal logic
    
    Preparation for potential conversion of DocumentHandler class into an abstract interface

--- core/documenthandler.hpp
+++ core/documenthandler.hpp
@@ -10,18 +10,27 @@
 
 namespace Yate::Core {
 
+
 class DocumentHandler : public Api::IObject {
 private:
     std::map<std::string, Document> m_documents;
     Api::IFileIOFactory& m_fileio_factory;
 
+    bool open_file(const std::string& filepath, const std::string& fileio);
+    bool close_file(const std::string& filepath, const std::string& fileio);
+    bool save_file(const std::string& filepath, const std::string& fileio);
 public:
+    enum Operation {
+        NOP,
+        OPEN,
+        CLOSE,
+        SAVE
+    };
     explicit DocumentHandler(Api::IFileIOFactory& fileio_factory);
 
     void update();
-    void open_file(const std::string& filepath, const std::string& fileio);
-    void close_file(const std::string& filepath);
 
+    void onFileCommand(const Utils::Event& event);
     void onOpenFileCommand(const Utils::Event& event);
     void onCloseFileCommand(const Utils::Event& event);
     void onDocumentChangeCommand(const Utils::Event& event);
--- core/src/documenthandler.cpp
+++ core/src/documenthandler.cpp
@@ -7,6 +7,31 @@ namespace Yate::Core {
 using namespace Utils;
 constexpr const std::string DOMAIN = "YCORE::DOCH";
 
+bool DocumentHandler::open_file(const std::string& filepath, const std::string& fileio){
+    if(m_documents.find(filepath) != m_documents.end()){
+        log(Log_t::INFO, DOMAIN, "Document %s already opened, nothing to do.\n", filepath.c_str());
+        return false;
+    }
+    log(Log_t::DEBUG, DOMAIN, "Open document...\n");
+    std::unique_ptr<Core::Api::FileReader> filereader = m_fileio_factory.create_reader(fileio);
+    m_documents.insert(std::make_pair(filepath, filereader->read(filepath)));
+    log(Log_t::DEBUG, DOMAIN, "Document %s opened, notify observers\n", filepath.c_str());
+    return true;
+}
+
+bool DocumentHandler::close_file(const std::string& filepath, const std::string& fileio){
+    if(m_documents.find(filepath) == m_documents.end()){
+        log(Log_t::INFO, DOMAIN, "Document %s already closed, nothing to do.\n", filepath.c_str());
+        return false;
+    }
+    return false;
+}
+
+bool DocumentHandler::save_file(const std::string& filepath, const std::string& fileio){
+    //emit_event("file_saved", m_documents.at(filepath));
+    return false;
+}
+
 DocumentHandler::DocumentHandler(Api::IFileIOFactory& fileio_factory)
     : Api::IObject("DocumentHandler"),
       m_documents(),
@@ -20,50 +45,85 @@ void DocumentHandler::update()
     }
 }
 
-void DocumentHandler::open_file(const std::string& filepath, const std::string& fileio){
-    if(m_documents.find(filepath) == m_documents.end()){
-        log(Log_t::DEBUG, DOMAIN, "Open document...\n");
-        std::unique_ptr<Core::Api::FileReader> filereader = m_fileio_factory.create_reader(fileio);
-        m_documents.insert(std::make_pair(filepath, filereader->read(filepath)));
-        log(Log_t::DEBUG, DOMAIN, "Document %s opened, notify observers\n", filepath.c_str());
-        emit_event("open_file_list_change", m_documents);
-    } else {
-        log(Log_t::DEBUG, DOMAIN, "Document already opened, nothing to do.\n");
+void DocumentHandler::onFileCommand(const Utils::Event& event){
+    if(event.arguments.size() != 3) {
+        log(Log_t::ERROR, DOMAIN, "%s Callback error, wrong number of args\n", event.name.c_str());
+        return;
     }
-    emit_event("file_opened", m_documents[filepath]);
-}
+    log(Log_t::INFO, DOMAIN, "%s Callback received\n", event.name.c_str());
+    auto& arguments = event.arguments;
 
-void DocumentHandler::close_file(const std::string& filepath){
-    emit_event("open_file_list_change", m_documents);
+    Operation docop = Operation::NOP;
+    std::string filename = "";
+    std::string fileio = "";
+
+    try {
+        docop = std::any_cast<Operation>(arguments[0]);
+        filename = std::any_cast<const std::string&>(arguments[1]);
+        fileio = std::any_cast<const std::string&>(arguments[2]);
+        log(Log_t::SILLY, DOMAIN, "Callback data casted\n");
+    } catch(const std::bad_any_cast& e) {
+        log(Log_t::ERROR, DOMAIN, "Error: %s\n", e.what());
+        return;
+    }
+
+    bool hasListChange = false;
+    switch(docop){
+        case Operation::OPEN:
+            hasListChange = open_file(filename, fileio);
+            emit_event("file_opened", m_documents[filename]);
+            break;
+        case Operation::CLOSE:
+            hasListChange = close_file(filename, fileio);
+            emit_event("file_closed", filename);
+            break;
+        case Operation::SAVE:
+            save_file(filename, fileio);
+            emit_event("file_saved", filename);
+            break;
+        default:
+            log(Log_t::ERROR, DOMAIN, "Error: Not a valid doc handl op: %d\n", docop);
+            return;
+    }
+    if(hasListChange){
+        emit_event("open_file_list_change", m_documents);
+    }
+
 }
 
 void DocumentHandler::onOpenFileCommand(const Utils::Event& event){
+    if(event.arguments.size() != 2) {
+        log(Log_t::ERROR, DOMAIN, "onOpenFile Callback error, wrong number of args\n");
+        return;
+    }
     log(Log_t::INFO, DOMAIN, "onOpenFile Callback received\n");
     auto& arguments = event.arguments;
-    if (arguments.size() == 2) {
-        try {
-            auto filename = std::any_cast<const std::string&>(arguments[0]);
-            auto fileio = std::any_cast<const std::string&>(arguments[1]);
-            // Verwendung der Map...
-            log(Log_t::SILLY, DOMAIN, "Callback data casted\n");
-            //std::shared_ptr<Core::Api::IFileReader> reader = m_fileio_factory.get_reader();
-            open_file(filename, fileio);
-        } catch(const std::bad_any_cast& e) {
-            log(Log_t::ERROR, DOMAIN, "Error: %s\n", e.what());
-        }
+
+
+
+    try {
+        auto filename = std::any_cast<const std::string&>(arguments[0]);
+        auto fileio = std::any_cast<const std::string&>(arguments[1]);
+        log(Log_t::SILLY, DOMAIN, "Callback data casted\n");
+        open_file(filename, fileio);
+    } catch(const std::bad_any_cast& e) {
+        log(Log_t::ERROR, DOMAIN, "Error: %s\n", e.what());
     }
 }
 
 void DocumentHandler::onCloseFileCommand(const Utils::Event& event){
+    if(event.arguments.size() != 2) {
+        log(Log_t::ERROR, DOMAIN, "onCloseFile Callback error, wrong number of args\n");
+        return;
+    }
     log(Log_t::INFO, DOMAIN, "onCloseFile Callback received\n");
     auto& arguments = event.arguments;
     if (arguments.size() == 2) {
         try {
             auto filename = std::any_cast<const std::string&>(arguments[0]);
-            auto filereader_name = std::any_cast<const std::string&>(arguments[1]);
-            // Verwendung der Map...
+            auto fileio = std::any_cast<const std::string&>(arguments[1]);
             log(Log_t::SILLY, DOMAIN, "Callback data casted\n");
-            close_file(filename/*, m_fileio_factory.get_reader(filereader_name)*/);
+            close_file(filename, fileio);
         } catch(const std::bad_any_cast& e) {
             log(Log_t::ERROR, DOMAIN, "Error: %s\n", e.what());
         }
